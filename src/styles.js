41ec39ccc949133e9bb55e7dc4d8e83902c1d5ba
alessandro lorenz <alessandro.lorenz@compasso.com.br>
2020-04-14 13:32:29 -0300

    Layout improvement

--- src/styles.js
+++ src/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div `
 /* tudo centralizado */
-  margin-top: 90px;
   height: 100%;
   display: flex;
   justify-content: center;
