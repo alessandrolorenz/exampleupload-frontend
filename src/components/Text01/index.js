38e3069fec7bb717846fd20fdcb8699370ed9797
alessandro lorenz <alessandro.lorenz@compasso.com.br>
2020-04-15 13:15:44 -0300

    Big Fix

--- src/components/Text01/index.js
+++ src/components/Text01/index.js
@@ -10,7 +10,7 @@ export default class Text01 extends Component {
     <Paragraph>Foi feito desde a criação de um backend com NodeJS, MongoDB Cloud, configuração de ambiente de desenvolvimento e de produção, variaveis de ambiente permitindo rodar o projeto localmente gravando no próprio computador.  O frontend foi desenvolvido com ReactJS e depois foi configurado para rodar backend e frontend no Heroku com deploy automático ligado com o github. Tudo isso foi feito seguindo o tutorual da Rocketseat: <a href="https://www.youtube.com/watch?v=MkkbUfcZUZM">https://www.youtube.com/watch?v=MkkbUfcZUZM</a>.</Paragraph>
     <Paragraph>Foi um exercício muito legal que envolveu uma porção de tecnologias bem bacanas. Fiz algumas customização e aprendi bastante. Muito bacana. </Paragraph>
     <h3>Tnx Rocketsear!!!</h3>
-    <small><a href="alessandrolorenz.com">alessandrolorenz.com</a><br></br></small>
+    <small><a href="http://www.alessandrolorenz.com">alessandrolorenz.com</a><br></br></small>
     <small><h4>github: </h4><a href="https://github.com/alessandrolorenz/uploadexample-backend">alessandrolorenz/uploadexample-backend</a><br></br></small>
     <small><a href="https://github.com/alessandrolorenz/exampleupload-frontend">alessandrolorenz/exampleupload-frontend</a></small>
     </Content>
